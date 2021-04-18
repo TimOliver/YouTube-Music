917f0d74a220c1b52c2e463872fdddc48d3730ea
Tim Oliver <me@timoliver.com.au>
2021-04-18 16:33:57 +0900

    Changed formatting

--- fastlane/Fastfile.swift
+++ fastlane/Fastfile.swift
@@ -43,8 +43,8 @@ class Fastfile: LaneFile {
     func releaseLane() {
         desc("Build, sign, notarize and release a new version of this macOS app")
 
-        sh(command: "echo '::set-output name=artifact_name::YouTube.zip'", log: false)
-        sh(command: "echo '::set-output name=artifact_hash::000000'", log: false)
+        sh(command: "echo '::set-output name=artifact-name::YouTube.zip'", log: false)
+        sh(command: "echo '::set-output name=artifact-hash::000000'", log: false)
         return
 
         // Get the info we need from the environment
